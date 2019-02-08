48b0f83f3f9c18d8ea68ba2eaf07bbd03b152e4f
Vladislav <vladikbirkos@yandex.ru>
2019-02-08 11:02:46 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int n = 11;
+		int a = 1, b = 1;
+		System.out.print(a + " " + b);
+		int fib = 2, i = 2;
+		while (i < n) {
+			fib = a + b;
+			a = b;
+			b = fib;
+			System.out.print(" " + fib);
+			i++;
+		}
+	}
+}
